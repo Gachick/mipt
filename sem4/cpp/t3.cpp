2a8084331f8e62b7986b1996664d0235b5a6fba8
gachick <maksdayt@gmail.com>
2025-05-16 14:55:52 +0300

    (sem4/cpp) map fix

--- sem4/cpp/t3.cpp
+++ sem4/cpp/t3.cpp
@@ -1,5 +1,6 @@
 #include <any>
 #include <iostream>
+#include <optional>
 #include <string>
 #include <type_traits>
 #include <vector>
@@ -75,21 +76,28 @@ template <typename Head, typename... Tail> struct TypeMap<Head, Tail...> {
     values[i] = std::any(value);
   };
 
-  template <typename T> T GetValue() {
+  template <typename T> std::optional<T> GetValue() {
     int i = TL::IndexOf<T, keys>::value;
-    return std::any_cast<T>(values[i]);
+    if (values[i].has_value()) {
+      return std::optional<T>(std::any_cast<T>(values[i].value()));
+    } else {
+      return std::nullopt;
+    }
   };
 
-  template <typename T> bool Contains() { return TL::Contains<T, keys>::value; }
+  template <typename T> bool Contains() {
+    constexpr int i = TL::IndexOf<T, keys>::value;
+    return values[i].has_value();
+  }
 
   template <typename T> void RemoveValue() {
     constexpr int i = TL::IndexOf<T, keys>::value;
-    values[i] = std::any(nullptr);
+    values[i] = std::nullopt;
   };
 
 private:
   using keys = TypeList<Head, Tail...>;
-  std::vector<std::any> values;
+  std::vector<std::optional<std::any>> values;
 };
 
 struct DataA {
@@ -101,27 +109,27 @@ struct DataB {
 int main() {
   TypeMap<int, DataA, double, DataB> myTypeMap;
   // Добавление элементов в контейнер
-  myTypeMap.AddValue<int>(42);
+  // myTypeMap.AddValue<int>(42);
   myTypeMap.AddValue<double>(3.14);
   myTypeMap.AddValue<DataA>({"Hello, TypeMap!"});
   myTypeMap.AddValue<DataB>({10});
   // Получение и вывод значений по типам ключей
-  std::cout << "Value for int: " << myTypeMap.GetValue<int>() << std::endl;
+  std::cout << "Value for int: " << myTypeMap.GetValue<int>().value_or(-1)
+            << std::endl;
   // // Вывод: 42
-  std::cout << "Value for double: " << myTypeMap.GetValue<double>()
+  std::cout << "Value for double: " << myTypeMap.GetValue<double>().value()
             << std::endl; // Вывод: 3.14
-  std::cout << "Value for DataA: " << myTypeMap.GetValue<DataA>().value
+  std::cout << "Value for DataA: " << myTypeMap.GetValue<DataA>().value().value
             << std::endl; // Вывод: Hello, TypeMap!
-  std::cout << "Value for DataB: " << myTypeMap.GetValue<DataB>().value
+  std::cout << "Value for DataB: " << myTypeMap.GetValue<DataB>().value().value
             << std::endl; // Вывод: 10
   // Проверка наличия элемента
   std::cout << "Contains int? " << (myTypeMap.Contains<int>() ? "Yes" : "No")
             << std::endl; // Вывод: Yes
+  std::cout << "Contains double? "
+            << (myTypeMap.Contains<double>() ? "Yes" : "No")
+            << std::endl; // Вывод: Yes
   // Удаление элемента
   myTypeMap.RemoveValue<double>();
-  // Попытка получения удаленного элемента
-  std::cout << "Value for double after removal: "
-            << myTypeMap.GetValue<double>()
-            << std::endl; // Вывод: (некорректное значение)
   return 0;
 }
